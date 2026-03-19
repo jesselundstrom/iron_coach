b149741957b6646fb9fd970436b47f4f2da654a4
Jesse Lundström <jesselundstrom@gmail.com>
2026-03-19 22:44:34 +0200

    Add loadData window typings

--- global.d.ts
+++ global.d.ts
@@ -4,5 +4,9 @@ declare global {
   interface Window {
     __IRONFORGE_APP_SHELL_READY__?: boolean;
     syncRuntimeStoreFromLegacy?: () => void;
+    loadData?: (options?: {
+      allowCloudSync?: boolean;
+      userId?: string;
+    }) => Promise<void> | void;
   }
 }
